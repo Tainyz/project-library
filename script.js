3d8232a8f53c5fc7bcd7dba1dbe62c90f46fc0b4
Tamtai <107045052+Tainyz@users.noreply.github.com>
2024-09-26 18:20:13 +0700

    boilerplate

---
+++ script.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(name,author,rating) {
+  this.name = name;
+  this.author = author;
+  this.rating = rating;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
